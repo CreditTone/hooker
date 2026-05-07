88ee48d6d4d6e98074165638a44bf698288c869e
stephen <stephen@stephendeMacBook-Air.local>
2026-05-07 18:10:59 +0800

    优化hook_artmethod_register.js

--- js/hook_artmethod_register.js
+++ js/hook_artmethod_register.js
@@ -1,14 +1,16 @@
+var TARGET_SO = "";
+
 function hook_ArtMethodRegister() {
     var symbols = Module.enumerateSymbolsSync("libart.so");
+    var targetSo = TARGET_SO;
 
     var ArtMethodRegisterNative = null;
-    var ArtMethod_PrettyMethod = null;
+    var JniRegisterNatives = null;
+    var nativeMethodMap = {};
     for (var i = 0; i < symbols.length; i++) {
         var symbol = symbols[i];
         var address = symbol.address;
         var name = symbol.name;
-        var indexArtMethod = name.indexOf("ArtMethod");
-        //_ZN3art9ArtMethod14RegisterNativeEPKv android 10
         if (
             name.indexOf("ArtMethod") >= 0 &&
             name.indexOf("RegisterNative") >= 0 &&
@@ -17,43 +19,78 @@ function hook_ArtMethodRegister() {
             console.log("ArtMethod::RegisterNative is at ", address, name);
             ArtMethodRegisterNative = address;
         }
-        if (indexArtMethod >= 0 && name.indexOf("PrettyMethod") >= 0 && name.indexOf("Eb") >= 0) {
-            console.log("ArtMethod::PrettyMethod is at ", address, name);
-            ArtMethod_PrettyMethod = address;
+        if (
+            name.indexOf("art") >= 0 &&
+            name.indexOf("JNI") >= 0 &&
+            name.indexOf("RegisterNatives") >= 0 &&
+            name.indexOf("CheckJNI") < 0
+        ) {
+            console.log("JNI::RegisterNatives is at ", address, name);
+            JniRegisterNatives = address;
         }
     }
-    var module_libext = null;
-    if (Process.arch === "arm64") {
-        module_libext = Module.load("/data/data/com.smile.gifmaker/files/libext64.so");
-    } else if (Process.arch === "arm") {
-        module_libext = Module.load("/data/data/com.smile.gifmaker/files/libext.so");
+    if (!ArtMethodRegisterNative) {
+        console.log("[!] ArtMethod::RegisterNative not found");
+        return;
     }
-    if (module_libext != null) {
-        var addr_PrettyMethod = module_libext.findExportByName("PrettyMethod");
-        var PrettyMethod = new NativeFunction(addr_PrettyMethod, "void", ["pointer", "pointer", "pointer", "int"]);
+    if (targetSo) {
+        console.log("[*] SO filter enabled: " + targetSo);
+    } else {
+        console.log("[*] SO filter disabled");
+    }
 
-        if (ArtMethodRegisterNative) {
-            //var foo_ArtMethod_PrettyMethod = new NativeFunction(ArtMethod_PrettyMethod, "pointer", ["pointer", "int"]);
-            Interceptor.attach(ArtMethodRegisterNative, {
-                onEnter: function (args) {
-                    try {
-                        var result = Memory.alloc(0x100);
-                        var fnPtr_ptr = args[1];
-                        var find_module = Process.findModuleByAddress(fnPtr_ptr);
-                        var offset = ptr(fnPtr_ptr).sub(find_module.base)
-                        PrettyMethod(ArtMethod_PrettyMethod, args[0], result, 0x100);
-                        console.log("[ArtMethod_RegisterNative] Method_sig:", result.readCString(), "module_name:", find_module.name, "offset:", offset);
-                    } catch (error) {
-                        console.log(error);
+    if (JniRegisterNatives) {
+        Interceptor.attach(JniRegisterNatives, {
+            onEnter: function (args) {
+                try {
+                    var javaClass = args[1];
+                    var className = Java.vm.tryGetEnv().getClassName(javaClass);
+                    var methodsPtr = ptr(args[2]);
+                    var methodCount = parseInt(args[3]);
+
+                    for (var i = 0; i < methodCount; i++) {
+                        var item = methodsPtr.add(i * Process.pointerSize * 3);
+                        var namePtr = Memory.readPointer(item);
+                        var sigPtr = Memory.readPointer(item.add(Process.pointerSize));
+                        var fnPtr = Memory.readPointer(item.add(Process.pointerSize * 2));
+                        var methodName = Memory.readCString(namePtr);
+                        var sig = Memory.readCString(sigPtr);
+                        nativeMethodMap[fnPtr.toString()] = className + "." + methodName + sig;
                     }
-
-                }, onLeave: function (retval) {
-
+                } catch (e) {
+                    console.log("[!] RegisterNatives map build failed: " + e);
                 }
-            });
-        }
+            }
+        });
+    } else {
+        console.log("[!] JNI::RegisterNatives not found, method names may be unavailable");
     }
+
+    Interceptor.attach(ArtMethodRegisterNative, {
+        onEnter: function (args) {
+            try {
+                // ArtMethod::RegisterNative(void* native_method)
+                // args[0] = ArtMethod*, args[1] = fnPtr
+                var artMethod = args[0];
+                var fnPtr = args[1];
+                if (fnPtr.isNull()) return;
+
+                var find_module = Process.findModuleByAddress(fnPtr);
+                if (!find_module) return;
+                if (targetSo && find_module.name !== targetSo) return;
+
+                var offset = fnPtr.sub(find_module.base);
+                var methodName = nativeMethodMap[fnPtr.toString()] || "(unresolved)";
+
+                console.log("[RegisterNative] " + methodName);
+                console.log("  SO:   " + find_module.name);
+                console.log("  fn:   " + fnPtr);
+                console.log("  off:  0x" + offset.toString(16));
+            } catch (error) {
+                console.log("[!] " + error);
+            }
+        }
+    });
 }
 
 setImmediate(hook_ArtMethodRegister)
-
